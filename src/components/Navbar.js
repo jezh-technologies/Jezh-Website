1f5ebd8d1647e8a9b256a325aa73a1f4bfa8246b
Jezh Technologies <114339951+jezh-technologies@users.noreply.github.com>
2023-07-09 15:13:58 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ export default function Navbar() {
 
 
 
-<a type="button" class="nav-link fw-bold text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">
+<a type="button" class="nav-link fw-bold text-white fs-5" data-bs-toggle="modal" data-bs-target="#exampleModal">
 Products
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill ps-1" viewBox="0 0 16 16">
 <path d="M7.247 11.14 2.451 5.658C1.88 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
@@ -251,7 +251,7 @@ Services
 
           </li> */}
           <li class="nav-item px-2 mx-1 dropdown">
-            <a class="nav-link fw-bold text-white dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link fw-bold text-white dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Services
             </a>
             <ul class="dropdown-menu rounded-4 p-2 bg-body">
@@ -286,7 +286,7 @@ Services
             </ul>
           </li>
           <li class="nav-item px-2 mx-1 dropdown">
-            <a class="nav-link fw-bold text-white dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link fw-bold text-white dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Company
             </a>
             <ul class="dropdown-menu rounded-4 p-2 bg-body">
@@ -324,7 +324,7 @@ Services
 
         <ul class="navbar-nav mx-end">
           <li class="nav-item">
-            <a class="btn btn-light rounded-pill fw-bold px-4" href="/contact">Contact</a>
+            <a class="btn btn-light rounded-pill fw-bold px-4 fs-5" href="/contact">Contact</a>
           </li>
         </ul>
       </div>
